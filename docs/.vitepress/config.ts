029ef6d5a2fcfa4771466ba280ea203ad8614079
AuTsing <763025696@qq.com>
2022-10-27 05:12:36 +0800

    add framework

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+    title: 'Denort',
+    description: 'Doc of Denort',
+    themeConfig: {
+        siteTitle: 'Denort',
+        nav: [
+            { text: '开始', link: '/guide/introduction' },
+            { text: 'API 文档', link: '/api/' },
+            { text: '关于', link: '/about' },
+        ],
+        sidebar: [
+            {
+                text: '开始',
+                items: [
+                    { text: '简介', link: '/guide/introduction' },
+                    { text: '快速上手', link: '/guide/quick-start' },
+                ],
+            },
+            {
+                text: 'API 文档',
+                items: [
+                    { text: 'io 输入输出', link: '/api/io' },
+                    { text: 'finger 手指', link: '/api/finger' },
+                ],
+            },
+        ],
+    },
+});
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+---
+layout: home
+
+hero:
+    name: Denort
+    text: 安卓自动化工作平台
+    tagline: 安卓首个基于 Deno 引擎的 JavaScript 和 TypeScript 运行时
+    image:
+        src: /assets/logo.png
+        alt: Denort
+    actions:
+        - theme: brand
+          text: 快速上手
+          link: /guide/quick-start
+        - theme: alt
+          text: 下载 APP
+---
